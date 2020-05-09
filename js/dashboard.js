c65efcee4a729d5f8c0434e068615d59ee324f54
rbtote <a01366943@itesm.mx>
2020-05-09 16:59:17 -0500

    datos ligas dashboard desplegados

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,23 @@
 $('#NameEmpresa').text(window.localStorage.empresa);
+idE = window.localStorage.empresaId;
+tipo = window.localStorage.tipo;
+
+function getDatosDashboard(){
+    $.ajax({
+        url: 'https://ligascortas.herokuapp.com/ligasVisitas/'+idE,      
+        headers: {
+            'Content-Type':'application/json'
+        },
+        method: 'GET',
+        success: function(data){     
+            console.log(data)
+            $('#totalesLigas').text(data.length)            
+        },
+        error: function(error_msg) {
+          alert((error_msg["responseText"]))
+        }
+    })
+}
+
+
+window.onload = getDatosDashboard;
