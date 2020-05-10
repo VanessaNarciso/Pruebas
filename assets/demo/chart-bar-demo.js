1b9e3f0a61e4144a24d9de36e1c3fb0bd0cc7a00
rbtote <a01366943@itesm.mx>
2020-05-09 19:01:44 -0500

    datos ligas dashboard grafica 1

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -1,46 +1,70 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
+idE = window.localStorage.empresaId;
 
-// Bar Chart Example
-var ctx = document.getElementById("myBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Chrome", "Firefox", "Safari", "Edge", "Opera", "Otros"],
-    datasets: [{
-      label: "Visitas",
-      backgroundColor: "rgba(62, 204, 142, 1)",
-      borderColor: "rgba(62, 204, 142, 1)",
-      data: [9821, 5312, 6343, 3458, 1234, 2984],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
-  }
-});
+
+function getDatosDashboard(){
+  $.ajax({
+      url: 'https://ligascortas.herokuapp.com/ligasVisitasNav/'+idE,      
+      headers: {
+          'Content-Type':'application/json'
+      },
+      method: 'GET',
+      success: function(data){     
+          console.log(data)
+          // Bar Chart Example
+          navs = [];
+          visitas = [];
+          for(var i=0; i<data.length; i++){
+            navs.push(data[i]._id)
+            visitas.push(data[i].visitas)
+          }
+          var ctx = document.getElementById("myBarChart");
+          var myLineChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: navs,
+              datasets: [{
+                label: "Visitas",
+                backgroundColor: "rgba(62, 204, 142, 1)",
+                borderColor: "rgba(62, 204, 142, 1)",
+                data: visitas,
+              }],
+            },
+            options: {
+              scales: {
+                xAxes: [{
+                  time: {
+                    unit: 'month'
+                  },
+                  gridLines: {
+                    display: false
+                  },
+                  ticks: {
+                    maxTicksLimit: 6
+                  }
+                }],
+                yAxes: [{
+                  ticks: {
+                    min: 0,
+                    max: 15000,
+                    maxTicksLimit: 5
+                  },
+                  gridLines: {
+                    display: true
+                  }
+                }],
+              },
+              legend: {
+                display: false
+              }
+            }
+          });
+
+      },
+      error: function(error_msg) {
+        alert((error_msg["responseText"]))
+      }
+  })
+}
