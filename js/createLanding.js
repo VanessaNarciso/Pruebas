62db5032944310fe08895b77dad3cf5becaebecd
Katia Tarin <45047009+ktarin@users.noreply.github.com>
2020-05-19 22:33:20 -0500

    attempt to make ligacorta
    still no image...

--- js/createLanding.js
+++ js/createLanding.js
@@ -1,5 +1,53 @@
 let serverName = 'https://ligascortas.herokuapp.com'
 
+function stringValue(str){
+  let ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let sum = 0;
+  mult = Math.floor(Math.random() * 1000) + 1;
+  mult2 = Math.floor(Math.random() * 151) + 1;
+  for(let i=0; i<str.length; i++){
+    sum=sum+(ALPHABET.indexOf(str[i])*mult2);
+  }  
+  return sum*mult*mult2;
+}
+
+function encode(ligaLarga) {
+  ligaLarga.replace(/[^0-9a-z]/gi, '');
+  num = stringValue(ligaLarga)
+  let ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let BASE     = ALPHABET.length;
+  sb = '';
+  while ( num > 1 ) {
+      sb = sb+ALPHABET[Math.ceil(num%BASE)];
+      num = Math.ceil(num/BASE);
+  }
+  return sb.split("").reverse().join("");
+}
+
+function loadEmpresas() {
+  $.ajax({
+    url: serverName+'/getCompanies',
+    // url: 'https://tuapp.herokuapp.com/todos',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+
+      for( let i = 0; i < data.length; i++) {
+        // aqui va su código para agregar los elementos de la lista
+        var id = data[i]['_id']
+        var nombre = data[i]['nombre']
+        $('#inputEmpresas').append("<option value='"+id+"'>"+nombre+"</option>")
+    
+      }
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+}
+
+loadEmpresas()
+
 $('#create_button').on('click', function(){    
     // cargar datos del form    
     let nombreLanding = $('#inputName').val()
@@ -8,6 +56,8 @@ $('#create_button').on('click', function(){
     let templateChoice = $("input[name='options']:checked").val();
     // id = "optionOne"
     // id = "optionTwo"
+    // how tf do u send an image i swear to god 
+    let ligaCorta = serverName+'/'+templateChoice+'/'+nombreLanding;
     let company = window.localStorage.empresaId
     let uId = window.localStorage.uid
 
@@ -16,6 +66,7 @@ $('#create_button').on('click', function(){
       "descriptionLanding": descriptionLanding,
       "footerLanding": footerLanding,
       "templateChoice" : templateChoice,
+      "ligaCorta" : ligaCorta,
       "empresaLiga" : company,
       "createdBy" : uId,
       "fechaCreacion" : new Date(),
@@ -34,10 +85,17 @@ $('#create_button').on('click', function(){
       data: json_to_send,
       success: function(data){
         // guardar token en localstorage o cookie        
-        window.location = 'consultUrl.html'
+        window.location = 'consultLanding.html'
       },
       error: function(error_msg) {
         alert((error_msg["responseText"]))
       }
     })
   })
+
+  window.onload = load();
+  
+  function load() {
+    $('#nombreLanding').val(serverName);
+  };
+  
