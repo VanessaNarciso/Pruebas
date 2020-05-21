afccaa32f261d248ba234032f30bcaa2fb628adf
rbtote <a01366943@itesm.mx>
2020-05-21 16:37:41 -0500

    get users if tipo 0  show empresa

--- js/getProfiles.js
+++ js/getProfiles.js
@@ -1,6 +1,15 @@
-function getUsersComp() {    
+function getUsersComp() {
+    var tipo = window.localStorage.tipo;
+    var serverName = 'https://ligascortas.herokuapp.com';
+    var getUrl = serverName;
+    if(tipo == "0"){
+      getUrl= getUrl+"/users/";
+      $('#usuariosEmpresa > thead tr').append('<th>Empresa</th>');
+    }else{
+      getUrl= getUrl+'/users/'+window.localStorage.empresaId;
+    }
     $.ajax({
-        url: 'https://ligascortas.herokuapp.com/users/'+window.localStorage.empresaId,      
+        url: getUrl,
         headers: {
             'Content-Type':'application/json'
         },
@@ -8,13 +17,24 @@ function getUsersComp() {
         success: function(data){
           let tabla = $("#bodyUsuarios");          
           for (let i = 0; i < data.length; i++) {
-            tabla.append(`                
+            if(tipo == "0"){
+              tabla.append(`                
                         <tr>                            
                             <td>${data[i].nombre}</td>
                             <td>${data[i].correo}</td>
                             <td>${data[i].numeroTelefono}</td>
+                            <td>${data[i].empresa}</td>
                         </tr>
                 `);
+            }else{
+              tabla.append(`                
+                        <tr>                            
+                            <td>${data[i].nombre}</td>
+                            <td>${data[i].correo}</td>
+                            <td>${data[i].numeroTelefono}</td>                            
+                        </tr>
+                `);
+            }
           }
           $('#usuariosEmpresa').DataTable({
             "language": {
