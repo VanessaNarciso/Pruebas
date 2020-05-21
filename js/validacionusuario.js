b562945bd1d8b6d314a9cd8dd50240c13495a045
rbtote <a01366943@itesm.mx>
2020-05-21 18:27:18 -0500

    validaAdmin

--- js/validacionusuario.js
+++ js/validacionusuario.js
@@ -2,7 +2,7 @@ if(window.localStorage.tipo == 1){
     // usuario tipo 1 es un administrador de empresas
     // no puede ver empresas
     document.getElementById("empresas").style.display = "none";
-    //document.getElementById("selectEmpresa").style.display = "none";
+    document.getElementById("selectEmpresa").style.display = "none";
     var str = document.getElementById("usertype").innerHTML; 
     var res = str.replace("Administración", "Empresa");
     document.getElementById("usertype").innerHTML = res;
@@ -11,7 +11,7 @@ if(window.localStorage.tipo == 2){
     // usuario tipo 2 es un miembro de una empresa
     // no puede ver los perfiles  ni empresas
     document.getElementById("empresas").style.display = "none";
-    //document.getElementById("selectEmpresa").style.display = "none";
+    document.getElementById("selectEmpresa").style.display = "none";
     document.getElementById("perfiles").style.display = "none";
     var str = document.getElementById("usertype").innerHTML; 
     var res = str.replace("Administración", "Usuario");
