bbfa7ea0986c3063c340eecd6769bee7ec7687c0
Katia Tarin <45047009+ktarin@users.noreply.github.com>
2020-05-20 22:38:44 -0500

    ahora con ruta correcta + usuario 0

--- js/getLandings.js
+++ js/getLandings.js
@@ -14,64 +14,65 @@ function dateRead(d){
     return date;
   }
   
-  function getLandingsComp() {    
-      $.ajax({
-        // cambiar al URL correcto. solo se necesita la info de configLanding 
-          url: 'https://ligascortas.herokuapp.com/configLanding/'+window.localStorage.empresaId,      
-          headers: {
-              'Content-Type':'application/json'
-          },
-          method: 'GET',
-          success: function(data){
-            let tabla = $("#bodyLandings");          
-            for (let i = 0; i < data.length; i++) {
-              $.ajax({
-                url: 'https://ligascortas.herokuapp.com/user/'+data[i].createdBy,      
-                headers: {
-                    'Content-Type':'application/json'
-                },
-                method: 'GET',
-                success: function(data2){
-                  fechaC = dateRead(new Date(data[i].fechaCreacion));
-                  fechaM = dateRead(new Date(data[i].fechaModificacion));
-                  /*
-                    <th>Id landing</th>
-                    <th>Título</th>
-                    <th>Descripción </th>
-                    <th>Fecha de modificación</th>
-                    <th>Liga corta</th>
-                    <th>Creador</th>
+function getLandingsComp() {
+  var tipo = window.localStorage.tipo;
+  var serverName = 'https://ligascortas.herokuapp.com';
+  var getUrl = serverName;
+    if(tipo == "0"){
+      getUrl= getUrl+"/landings/";
+      $('#creator_header').text('Empresa');
+    }else{
+      getUrl= getUrl+'/landings/'+window.localStorage.empresaId;
+    }
+    console.log(getUrl)
+    $.ajax({
+        url: getUrl,      
+        headers: {
+            'Content-Type':'application/json'
+        },
+        method: 'GET',
+        success: function(data){
+          console.log(data);
+          let tabla = $("#bodyLandings");          
+          for (let i = 0; i < data.length; i++) {
+                fechaC = dateRead(new Date(data[i].fechaCreacion));
+                ligOrg = data[i].ligaOriginal.substring(8,30)+' ...';   
+                /*
+                  <th>Nombre</th>
+                  th>Id landing</th>
+                  <th>Descripción </th>
+                  <th>Fecha de creación</th>
+                  <th>Liga</th>
+                  <th>Creador</th>
                   
-                  */
+                */                             
                   tabla.append(`                
-                          <tr>
-                              <td> ${data[i].landingId} </td>
-                              <td> ${data[i].nombreLanding} </td>
-                              <td> ${data[i].descriptionLanding}</td>
-                              <td>${fechaC}</td>
-                              <td>${fechaM}</td>  
-                              <td>${data2.nombre}</td>     
-                          </tr>
-                  `);
-                  if(i == data.length-1){
-                    $('#landingsEmpresa').DataTable({
-                      "language": {
-                          "lengthMenu": "Mostrar _MENU_ lansing pages por página",
-                          "zeroRecords": "No hay landing pages para mostrar",
-                          "info": "Página _PAGE_ de _PAGES_",
-                          "infoEmpty": "",
-                          "infoFiltered": "(Buscando en _MAX_ landing pages)"
-                      }
-                    });
-                  }
+                  <tr>
+                    <td> ${data[i].landingId} </td>
+                    <td> ${data[i].nombreLanding} </td>
+                    <td> ${data[i].descriptionLanding}</td>
+                    <td>${fechaC}</td>
+                    <td> <a href ="${data[i].ligaCorta}"> ${ligOrg}</d>
+                    <td>${data[i].creator}</td>     
+                    </tr>
+                `);              
+                if(i == data.length-1){
+                  $('#landingsEmpresa').DataTable({
+                    "language": {
+                        "lengthMenu": "Mostrar _MENU_ lansing pages por página",
+                        "zeroRecords": "No hay landing pages para mostrar",
+                        "info": "Página _PAGE_ de _PAGES_",
+                        "infoEmpty": "",
+                        "infoFiltered": "(Buscando en _MAX_ landing pages)"
+                    }
+                  });
                 }
-              })            
-            }          
-          },
-          error: function(error_msg) {
-            alert((error_msg["responseText"]))
           }
-        })
-  }
+        },
+        error: function(error_msg) {
+          alert((error_msg["responseText"]))
+        }
+      })
+}
   
-  window.onload = getLandingComp;
+  window.onload = getLandingsComp;
